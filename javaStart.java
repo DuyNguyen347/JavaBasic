fa581764ed9dd4798f515033dad5e1fa95d90259
= <=>
2022-01-25 23:28:26 +0700

    modified

--- javaStart.java
+++ javaStart.java
@@ -1,5 +1,13 @@
 public class javaStart {
     public static void main(String[] args){
-        System.out.print("Hello, I am Duy from Da Nang");
+        System.out.println("Hello, I am Duy from Da Nang");
+        System.out.println("I am 21 years old");
+        // sysout
+        int x =122;
+        System.out.println("MY number is : " +x);
+        float y = 3.112f;
+        System.out.println(y);
+        boolean z = false;
+        System.out.println(z);
     }
 }
