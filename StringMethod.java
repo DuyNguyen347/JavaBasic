667832873e47eebb6890baef7a4196ccad5d7309
= <=>
2022-01-28 23:20:13 +0700

    StringMethod

---
+++ StringMethod.java
@@ -0,0 +1,38 @@
+//import javax.swing.plaf.synth.SynthSplitPaneUI;
+//import javax.swing.text.StyledEditorKit.BoldAction;
+
+//import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
+public class StringMethod {
+    public static void main(String[] args){
+        String name = "Duy";
+        // compair 2 string
+        boolean result = name.equals("Duy");
+        System.out.println(result);
+        //length of string
+        int result1  = name.length();
+        System.out.println(result1);
+        // char at number index of string
+        char result2 = name.charAt(1);
+        System.out.println(result2);
+        // number index of X char
+        int result3 = name.indexOf('u');
+        System.out.println(result3); 
+        // test empty string
+        boolean result4 = name.isEmpty();
+        System.out.println(result4);
+        // upercase and lowercase
+        String result5 = name.toUpperCase();
+        String result6 = name.toLowerCase();
+        System.out.println(result5 + ' ' + result6);
+        // remove all empty space before and after string
+        String name2 = "    Duy Nguyen   ";
+        String result7 = name2.trim();
+        System.out.println(result7);
+        // replace char
+        String result8 = name.replace('y', 'Y');
+        System.out.println(result8);
+
+
+    }
+}
