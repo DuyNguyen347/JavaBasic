feec2f2e2da757b07e67b42981ed1fd7d928eac4
DuyNguyen347 <97797169+DuyNguyen347@users.noreply.github.com>
2022-02-01 23:02:07 +0700

    Update HappyNewYear.java
    Modified

--- HappyNewYear.java
+++ HappyNewYear.java
@@ -1,6 +1,6 @@
 public class HappyNewYear {
     public static void main(String[] args){
-        System.out.println("Happy New Year");
+        System.out.println("Happy New Year 2022");
         System.out.println("In this New Year, I wish that all my wishes are fullfilled");
     }
 }
