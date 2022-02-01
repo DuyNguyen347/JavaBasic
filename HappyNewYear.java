a1ad765560cc73fcf7768380f5dce441d517d122
= <=>
2022-02-01 23:00:49 +0700

    Happy New year

---
+++ HappyNewYear.java
@@ -0,0 +1,6 @@
+public class HappyNewYear {
+    public static void main(String[] args){
+        System.out.println("Happy New Year");
+        System.out.println("In this New Year, I wish that all my wishes are fullfilled");
+    }
+}
