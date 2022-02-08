d7e330c54f9cb1c23a76550b5681e2e6a95363e8
= <=>
2022-02-08 10:45:30 +0700

    update

--- StringMethod.java
+++ StringMethod.java
@@ -5,33 +5,41 @@
 
 public class StringMethod {
     public static void main(String[] args){
-        String name = "Duy";
-        // compair 2 string
-        boolean result = name.equals("Duy");
-        System.out.println(result);
-        //length of string
-        int result1  = name.length();
-        System.out.println(result1);
-        // char at number index of string
-        char result2 = name.charAt(1);
-        System.out.println(result2);
-        // number index of X char
-        int result3 = name.indexOf('u');
-        System.out.println(result3); 
-        // test empty string
-        boolean result4 = name.isEmpty();
-        System.out.println(result4);
-        // upercase and lowercase
-        String result5 = name.toUpperCase();
-        String result6 = name.toLowerCase();
-        System.out.println(result5 + ' ' + result6);
-        // remove all empty space before and after string
-        String name2 = "    Duy Nguyen   ";
-        String result7 = name2.trim();
-        System.out.println(result7);
-        // replace char
-        String result8 = name.replace('y', 'Y');
-        System.out.println(result8);
+        // String name = "Duy";
+        // // compair 2 string
+        // boolean result = name.equals("Duy");
+        // System.out.println(result);
+        // //length of string
+        // int result1  = name.length();
+        // System.out.println(result1);
+        // // char at number index of string
+        // char result2 = name.charAt(1);
+        // System.out.println(result2);
+        // // number index of X char
+        // int result3 = name.indexOf('u');
+        // System.out.println(result3); 
+        // // test empty string
+        // boolean result4 = name.isEmpty();
+        // System.out.println(result4);
+        // // upercase and lowercase
+        // String result5 = name.toUpperCase();
+        // String result6 = name.toLowerCase();
+        // System.out.println(result5 + ' ' + result6);
+        // // remove all empty space before and after string
+        // String name2 = "    Duy Nguyen   ";
+        // String result7 = name2.trim();
+        // System.out.println(result7);
+        // // replace char
+        // String result8 = name.replace('y', 'Y');
+        // System.out.println(result8);
+         String[] animals = new String[4];
+        animals[0] = "cat";
+        animals[1] = "dog";
+        animals[2] = "bird";
+        animals[3] = "fish";
+        for(int i=0;i<animals.length;i++){
+            System.out.println(animals[i]);
+        }
 
 
     }
